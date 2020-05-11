204df6776c8d9c33a404baf272a07eb41e5c6aa6
Fylls <sergentifilippo@gmail.com>
2020-05-11 14:06:49 +0200

    fix
    was broken

--- static/JS/core.js
+++ static/JS/core.js
@@ -1,34 +1,25 @@
 //  Callenge 5: BlackJack
 
-// you don't have to write an onclkik in the HTML part
+// you don't have to write an onClick in the HTML part
 
 let blackJackGame = {
   you: { scoreSpan: "#your-bj-score", div: ".your-box", score: 0 },
   dealer: { scoreSpan: "#dealer-bj-score", div: ".dealer-box", score: 0 },
 };
+
 const YOU = blackJackGame.you;
 const DEALER = blackJackGame.dealer;
 
-hitButton = document.getElementById("bj-hit");
+// env variables
+const hitSound = new Audio("static/audio/swish.m4a");
+
+let hitButton = document.getElementById("bj-hit");
 hitButton.addEventListener("click", blackJackHit);
 
 function blackJackHit() {
   let cardImage = document.createElement("img");
   cardImage.src = "static/images/Q.png";
   document.querySelector(YOU.div).appendChild(cardImage);
+  hitSound.play();
+  console.log("cia0o");
 }
-
-/*
-  
-  
-  hitButton = document.getElementById("bj-hit");
-  hitButton.addEventListener("click", blackJackHit);
-  
-  function blackJackHit() {
-    let cardImage = document.createElement("img");
-    cardImage.src = "./static/images/Q.png";
-    console.log(cardImage);
-    document.querySelector(YOU.div).appendChild(cardImage);
-  }
-  
-  */
